95e5ba7af4c9e108bc3de7518ff4602791b6fdf5
jklaar <71821981+jklaar@users.noreply.github.com>
2023-09-07 11:09:17 +0200

    Add files via upload

---
+++ IntuneApps/7-zip/Source/Install.cmd
@@ -0,0 +1,18 @@
+Echo Off
+
+chcp 65001
+
+Color 70
+
+CLS
+
+Echo ------------------------------------------------------------
+Echo STÄNG INTE DETTA FÖNSTER!
+Echo Det kommer stängas automatiskt då installationen är klar.
+Echo.
+Echo \\ Atea
+Echo ------------------------------------------------------------
+Echo.
+Echo Installerar 7-zip...
+
+"%~dp07z2301-x64.exe"
---
+++ IntuneApps/7-zip/Source/Uninstall.cmd
@@ -0,0 +1,18 @@
+Echo Off
+
+chcp 65001
+
+Color 70
+
+CLS
+
+Echo ------------------------------------------------------------
+Echo STÄNG INTE DETTA FÖNSTER!
+Echo Det kommer stängas automatiskt då installationen är klar.
+Echo.
+Echo \\ Atea
+Echo ------------------------------------------------------------
+Echo.
+Echo Avinstallerar 7-zip...
+
+"C:\Program Files\7-Zip\Uninstall.exe"
